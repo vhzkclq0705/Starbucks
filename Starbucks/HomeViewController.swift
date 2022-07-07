1bd27398b4fccf02a2d24a0cec3b98d28f737ac4
o_jun <vhzkclq0705@naver.com>
2022-07-07 22:30:34 +0900

    [ADD] Resign 이미지 추가

--- Starbucks/HomeViewController.swift
+++ Starbucks/HomeViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topBanner: UIImageView!
+    @IBOutlet weak var topBannerHeight: NSLayoutConstraint!
+    
+    let maxHeight: CGFloat = 300
+    let minHeight: CGFloat = 40
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // test
+        scrollView.delegate = self
     }
-
-
 }
 
+extension HomeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollPosition = scrollView.contentOffset.y
+        let y = topBannerHeight.constant - scrollPosition
+        
+        if y > maxHeight {
+            topBannerHeight.constant = maxHeight
+        }
+        else if y < minHeight {
+            topBannerHeight.constant = minHeight
+            topBanner.alpha = 0
+        } else {
+            topBannerHeight.constant = y
+            topBanner.alpha = y / 300
+            
+            scrollView.contentOffset.y = 0
+        }
+    }
+}
